480546df352dcc1e7a2596df41bc571d0c30af13
mackowski <35339942+mackowski@users.noreply.github.com>
2025-06-06 11:04:37 +0200

    Update main.go

--- main.go
+++ main.go
@@ -1,1 +1,16 @@
-import "github.com/mackowski/ObfuscatedMethodPackage"
+package main
+
+import (
+	"fmt"
+	obfuscated "github.com/mackowski/ObfuscatedMethodPackage"
+)
+
+func main() {
+	// Example usage of the package
+	fmt.Println("Using ObfuscatedMethodPackage")
+	// Add your code here to use the package's functionality
+	err := obfuscated.ConnectToEncodedAddress("bG9jYWxob3N0OjQ0NDQ=")
+	if err != nil {
+		fmt.Println("Error:", err)
+	}
+}
